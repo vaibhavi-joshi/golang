27e7e08939b83a6655f10085365ae4c04cb56b98
Vaibhavi Joshi <Vaibhavi_Joshi@outlook.com>
2024-04-25 05:52:21 -0400

    switch statement

--- chapters/05-if-else.go
+++ chapters/05-if-else.go
@@ -69,5 +69,20 @@ func ifelse() {
 		}
 
 	}
+	// Switch statement example
+	city := "London"
+
+	switch city {
+	case "New York":
+		// booking for New York conference
+	case "Singapore", "Hong Kong":
+		// booking for Singapore & Hong Kong conference
+	case "London", "Berlin":
+		// booking for London & Berlin conference
+	case "Mexico City":
+		// booking for Mexico City conference
+	default:
+		fmt.Print("No valid city selected")
+	}
 
 }
